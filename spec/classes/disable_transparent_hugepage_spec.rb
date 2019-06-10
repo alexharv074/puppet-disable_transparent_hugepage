c93014446830976c824a3d621213fd4ece12dd08
Alex Harvey <alexharv074@gmail.com>
2019-06-11 01:47:46 +1000

    Fix failing unit tests
    Turns out it is necessary to mock the operatingsystem fact even
    in Puppet 6 if you use a service resource, because it
    references it in the source code here:
    
    https://github.com/puppetlabs/puppet/blob/b59a58d3f3b189809335e322df1b2b0dbb628fcf/lib/puppet/provider/service/base.rb#L18-L25

--- spec/classes/disable_transparent_hugepage_spec.rb
+++ spec/classes/disable_transparent_hugepage_spec.rb
@@ -3,18 +3,17 @@ require 'spec_helper'
 describe 'disable_transparent_hugepage' do
   context 'EL7' do
     let(:facts) {{
-      :osfamily                  => 'RedHat',
-      :operatingsystemmajrelease => '7',
+      'os' => {'family' => 'RedHat', 'release' => { 'major' => '7', 'minor' => '1', 'full' => '7.1.1503'}},
+      'operatingsystem' => 'Linux',
     }}
     it {
       is_expected.to compile.with_all_deps
     }
   end
-
   context 'Debian' do
     let(:facts) {{
-      :osfamily                  => 'Debian',
-      :operatingsystemmajrelease => '8',
+      'os' => {'family' => 'Debian', 'release' => {'major' => '8'}},
+      'operatingsystem' => 'Linux',
     }}
     it {
       is_expected.to compile.with_all_deps
