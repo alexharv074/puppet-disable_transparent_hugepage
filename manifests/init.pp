56737d66827fdbba5b5973a5c15a8da24ff038b5
Alex Harvey <alexharv074@gmail.com>
2019-06-11 18:34:22 +1000

    Update to latest MongoDB instructions
    Since I wrote this module, the MongoDB instructions for RHEL 6 have
    changed quite significantly.
    
    This refactors to align with the latest version of those instructions,
    after all checking other sources for this procedure, which I found to be
    mostly the same as the MongoDB version.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,8 @@ class disable_transparent_hugepage {
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
-    content => template("${module_name}/disable-transparent-hugepage.erb"),
+    content => template("$module_name/disable-transparent-hugepage.erb"),
+    before  => Service['disable-transparent-hugepage'],
   }
 
   service { 'disable-transparent-hugepage':
@@ -20,33 +21,52 @@ class disable_transparent_hugepage {
     enable   => true,
   }
 
-  File['/etc/init.d/disable-transparent-hugepage']
-  ->
-  Service['disable-transparent-hugepage']
-
   $family = $facts['os']['family']
   $major  = $facts['os']['release']['major']
 
-  if ($family == 'RedHat') and
-    (versioncmp($major, '6') >= 0) {
+  if ($family == 'RedHat') {
 
-    file { '/etc/tuned/custom':
-      ensure => directory,
+    package { 'tuned':
+      ensure => installed,
+      before => Exec['enable-tuned-profile'],
     }
 
-    file { '/etc/tuned/custom/tuned.conf':
-      ensure  => file,
-      content => template("${module_name}/tuned.conf.erb"),
+    $profile_name = 'custom'
+
+    case $major {
+      '7': {
+        file { "/etc/tuned/$profile_name":
+          ensure => directory,
+        }
+
+        file { "/etc/tuned/$profile_name/tuned.conf":
+          ensure  => file,
+          content => template("$module_name/el7/tuned.conf.erb"),
+          before  => Exec['enable-tuned-profile'],
+        }
+      }
+      '6': {
+        file { "/etc/tune-profiles/$profile_name":
+          ensure => directory,
+        }
+
+        ['sysctl.ktune','sysctl.s390x.ktune','ktune.sysconfig','tuned.conf'].each |$file| {
+          file { "/etc/tune-profiles/$file":
+            ensure  => file,
+            content => template("$module_name/el6/$file.erb"),
+            before  => Exec['enable-tuned-profile'],
+          }
+        }
+      }
+      default: {
+        fail("Unsupported OS ${facts['os']}")
+      }
     }
 
     exec { 'enable-tuned-profile':
-      command => 'tuned-adm profile custom',
+      command => "tuned-adm profile $profile_name",
       unless  => 'tuned-adm active | grep -q custom',
-      path    => '/sbin:/usr/bin',
+      path    => '/bin:/sbin:/usr/bin',
     }
-
-    File['/etc/tuned/custom/tuned.conf']
-    ->
-    Exec['enable-tuned-profile']
   }
 }
--- spec/classes/disable_transparent_hugepage_spec.rb
+++ spec/classes/disable_transparent_hugepage_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe 'disable_transparent_hugepage' do
+  context 'EL6' do
+    let(:facts) {{
+      'os' => {'family' => 'RedHat', 'release' => {'major' => '6', 'minor' => '6', 'full' => '6.6'}},
+      'operatingsystem' => 'Linux',
+    }}
+    it {
+      is_expected.to compile.with_all_deps
+    }
+  end
   context 'EL7' do
     let(:facts) {{
-      'os' => {'family' => 'RedHat', 'release' => { 'major' => '7', 'minor' => '1', 'full' => '7.1.1503'}},
+      'os' => {'family' => 'RedHat', 'release' => {'major' => '7', 'minor' => '1', 'full' => '7.1.1503'}},
       'operatingsystem' => 'Linux',
     }}
     it {
